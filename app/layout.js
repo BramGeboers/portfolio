04caeff6aa118744e742162f3e6a7ad191e055db
bramgeboers-UCLL <r0891987@ucll.be>
2024-04-01 01:37:18 +0200

    smooth scroll

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { Poppins } from 'next/font/google'
 import localfont from "next/font/local"
-import SmoothScrolling from "@/components/SmoothScrolling";
+// import SmoothScrolling from "@/components/SmoothScrolling";
 
 
 import "./globals.css";
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${mori.variable} font-mori`}>
       <body className={poppins.className}>
-        <SmoothScrolling>{children}</SmoothScrolling>
+        {children}
       </body>
     </html>
   );
